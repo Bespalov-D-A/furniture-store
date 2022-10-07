f87143d5b30e42549d08059866e6df97ebe5347b
leoleq <googlonator@gmail.com>
2022-10-07 19:11:52 +0400

    Fix small mistakes

--- tasks/js.js
+++ tasks/js.js
@@ -8,9 +8,7 @@ const settings = require('../config/settings.js')
 const plumber = require('gulp-plumber') // Выводим сообщения
 const notify = require('gulp-notify') // Перенаправляем сообщения в ОС
 const rename = require('gulp-rename') //Изменяем имя
-const size = require('gulp-size') // Показываем размер файла
 const babel = require('gulp-babel')
-const uglify = require('gulp-uglify')
 const webpack = require('webpack-stream')
 
 // processing
@@ -19,8 +17,6 @@ const js = () => {
     .pipe(plumber(settings.plumber(notify, 'JavaScript')))
     .pipe(babel())
     .pipe(webpack(settings.webpack.configuration))
-    .pipe(dest(path.js.dest, { sourcemaps: true }))
-    .pipe(uglify())
     .pipe(rename({ suffix: '.min' }))
     .pipe(dest(path.js.dest, { sourcemaps: true }))
 }
